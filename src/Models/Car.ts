de5fb1294bd96113cfa3b9cc1a8ec81b77c7db2e
daniel <danielrubensdaniel@gmail.com>
2023-04-23 13:55:47 -0300

    feat: car model

---
+++ src/Models/Car.ts
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+import ICar from '../Interfaces/ICar';
+
+const Car = new mongoose.Schema<ICar>({
+  model: { type: String, required: true },
+  year: { type: Number, required: true },
+  color: { type: String, required: true },
+  status: { type: Boolean, required: true },
+  buyValue: { type: Number, required: true },
+  doorsQty: { type: Number, required: true },
+  seatsQty: { type: Number, required: true },
+});
+
+export default mongoose.model('Car', Car);
