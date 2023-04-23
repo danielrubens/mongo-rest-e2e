e2444b43310b6fbdd95e21162fd636950b9503ea
daniel <danielrubensdaniel@gmail.com>
2023-04-23 18:51:13 -0300

    feat: app post cars route

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,10 @@
 import express from 'express';
+import controller from './Controllers/Car';
 
 const app = express();
+app.use(express.Router());
+app.use(express.json());
+
+app.post('/cars', controller.create);
 
 export default app;
