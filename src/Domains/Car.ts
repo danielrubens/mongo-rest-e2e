6563e9220c97d30778eac4ce7c62db603588a9db
daniel <danielrubensdaniel@gmail.com>
2023-04-24 00:40:51 -0300

    refactor: cars domain

--- src/Domains/Car.ts
+++ src/Domains/Car.ts
@@ -1,26 +1,48 @@
-import IVehicle from '../Interfaces/IVehicle';
+import ICar from '../Interfaces/ICar';
 
-class Car implements IVehicle {
-  constructor(
-    public model: string, 
-    public year: number, 
-    public color: string, 
-    public buyValue: number, 
-    public id?: string, 
-    public status?: boolean,
-  ) {}
+class Car {
+  public id: string | undefined;
+  protected model: string;
+  public year: number;
+  public color: string;
+  public buyValue: number;
+  public status: boolean | undefined;
+  public doorsQty: number;
+  public seatsQty: number;
+
+  constructor({ id, model, year, color, status, buyValue, doorsQty, seatsQty }: ICar) {
+    this.id = id;
+    this.buyValue = buyValue;
+    this.color = color;
+    this.model = model;
+    this.year = year;
+    this.status = status;
+    this.doorsQty = doorsQty;
+    this.seatsQty = seatsQty;
+  }
   
-  getId() { return this.id; }
+  public getId() {
+    return this.id;
+  }
   
-  getStatus() { return this.status; }
+  public getStatus() {
+    return this.status;
+  }
   
-  getBuyValue() { return this.buyValue; }
+  public getBuyValue() {
+    return this.buyValue;
+  }
   
-  getColor() { return this.color; }
-  
-  getModel() { return this.model; }
-  
-  getYear() { return this.year; }
+  public getColor() {
+    return this.color;
+  }
+  public getModel() {
+    return this.model;
+  }
+
+  public getYear() {
+    return this.year;
+  }
 }
-  
+
 export default Car;
