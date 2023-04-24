8fa13e2353ee2e5feef5f364ccca7cc388b920ae
daniel <danielrubensdaniel@gmail.com>
2023-04-24 18:27:10 -0300

    feat: send middleware

--- src/Middlewares/index.ts
+++ src/Middlewares/index.ts
@@ -1,11 +1,18 @@
+import { Response } from 'express';
 import { Types } from 'mongoose';
 
-const checkId = (id: string, found: any, vehicle: string) => {
+const send = (res: Response, object:any, method?: string) => {
+  if (method === 'ok') return res.status(200).json(object);
+  if (method === 'insert') return res.status(201).json(object);
+  res.status(object.code).json(object.message);
+};
+
+const checkId = (id: any, found: any, vehicle: string) => {
   if (!Types.ObjectId.isValid(id)) {
     return { code: 422, message: { message: 'Invalid mongo id' } };
-  }
+  } 
   if (!found) return { code: 404, message: { message: `${vehicle} not found` } };
   return false;
 };
 
-export default checkId;
+export { send, checkId };
