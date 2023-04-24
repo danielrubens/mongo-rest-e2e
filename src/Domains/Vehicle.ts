6de49647f9b114ca7c1e5d9910535dfbb5636a9b
daniel <danielrubensdaniel@gmail.com>
2023-04-24 18:11:10 -0300

    feat: vehicle domain

---
+++ src/Domains/Vehicle.ts
@@ -0,0 +1,44 @@
+import IVehicle from '../Interfaces/IVehicle';
+
+class Vehicle {
+  public id: string | undefined;
+  protected model: string;
+  public year: number;
+  public color: string;
+  public buyValue: number;
+  public status: boolean | undefined;
+
+  constructor({ id, model, year, color, status, buyValue }: IVehicle) {
+    this.id = id;
+    this.buyValue = buyValue;
+    this.color = color;
+    this.model = model;
+    this.year = year;
+    this.status = status;
+  }
+  
+  public getId() {
+    return this.id;
+  }
+  
+  public getStatus() {
+    return this.status;
+  }
+  
+  public getBuyValue() {
+    return this.buyValue;
+  }
+  
+  public getColor() {
+    return this.color;
+  }
+  public getModel() {
+    return this.model;
+  }
+
+  public getYear() {
+    return this.year;
+  }
+}
+
+export default Vehicle;
