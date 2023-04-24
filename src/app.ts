2f90a3e4b569c270b983853fe8cd965ae85b16ab
daniel <danielrubensdaniel@gmail.com>
2023-04-24 18:30:21 -0300

    feat: app routes

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
 import express from 'express';
-import controller from './Controllers/vehicles';
+import vehicles from './Controllers/vehicles';
 
 const app = express();
 app.use(express.Router());
 app.use(express.json());
 
-app.get('/cars', controller.getAll);
-app.post('/cars', controller.create);
+app.get('/cars', vehicles.getAll);
+app.get('/cars/:id', vehicles.getById);
+app.post('/cars', vehicles.create);
+app.put('/cars/:id', vehicles.update);
+
+app.get('/motorcycles', vehicles.getAll);
+app.get('/motorcycles/:id', vehicles.getById);
+app.post('/motorcycles', vehicles.create);
+app.put('/motorcycles/:id', vehicles.update);
 
 export default app;
