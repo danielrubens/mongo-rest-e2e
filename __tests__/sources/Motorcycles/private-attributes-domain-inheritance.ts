46337376f3c95ee49a21e374332930ecbf6938d8
daniel <danielrubensdaniel@gmail.com>
2023-04-25 01:55:43 -0300

    source: motorcycles private attributes domain inheritance

---
+++ __tests__/sources/Motorcycles/private-attributes-domain-inheritance.ts
@@ -0,0 +1,10 @@
+import Motorcycle from "../../../src/Domains/Motorcycle";
+
+class subMotorcycleTest extends Motorcycle{
+    public getCategory(){
+        return this.getCategory;
+    }
+    public getEngineCapacity(){
+        return this.getEngineCapacity;
+    }
+}
