763c6dabc27397e01471979f5bd1067ea2f8ba5d
daniel <danielrubensdaniel@gmail.com>
2023-04-25 14:48:35 -0300

    source: vehicle correct interface with status

---
+++ __tests__/sources/Vehicle/correct-interface-with-status.ts
@@ -0,0 +1,9 @@
+import { IVehicle } from "../../../src/Interfaces";
+
+const vehicleInterfaceTest: IVehicle = {
+    model: 'anymodel',
+    year: 2023,
+    color: 'anycolor',
+    status: true,
+    buyValue: 10000
+}
