9d94e6b9fd3a4d34955e6d0e12bc9cd23724bd90
daniel <danielrubensdaniel@gmail.com>
2023-04-25 14:57:39 -0300

    source: vehicle incorrect interface with motorcycle attributes

---
+++ __tests__/sources/Vehicle/incorrect-interface-with-motorcycle-attributes.ts
@@ -0,0 +1,12 @@
+import { IVehicle } from "../../../src/Interfaces";
+
+const vehicleInterfaceTest: IVehicle = {
+    model: 'anymodel',
+    year: 2023,
+    color: 'anycolor',
+    status: true,
+    buyValue: 10000,
+    category: 'anycategory',
+    engineCapacity: 3000
+
+}
