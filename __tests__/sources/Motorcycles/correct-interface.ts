4e6796083bfeca914976a64435f120a2136c0b0f
daniel <danielrubensdaniel@gmail.com>
2023-04-25 01:36:04 -0300

    source: motorcycles correct interface

---
+++ __tests__/sources/Motorcycles/correct-interface.ts
@@ -0,0 +1,30 @@
+import { IMotorcycle } from "../../../src/Interfaces";
+
+const motorcycleInterfaceTestCategory1: IMotorcycle = {
+    model: 'Suzuki Intruder 125',
+    year: 2000, 
+    color: 'Black',
+    status: true,
+    buyValue: 2.500,
+    category: 'Cruiser', 
+    engineCapacity: 125,
+  };
+  
+  const motorcycleInterfaceTestCategory2: IMotorcycle = {
+    model: 'Kawasaki KLR 650', 
+    year: 2005, 
+    color: 'Green',
+    buyValue: 15.000,
+    category: 'Dual-Sport',
+  };
+  
+  const motorcycleInterfaceTestCategory3: IMotorcycle = {
+    model: 'Harley-Davidson Street Bob', 
+    year: 2019, 
+    color: 'Orange',
+    status: false, 
+    buyValue: 22.000,
+    category: 'Cruiser',
+    engineCapacity: 1750,
+  };
+  
