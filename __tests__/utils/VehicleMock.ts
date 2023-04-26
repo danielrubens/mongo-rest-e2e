c7c4d20dfcb9dc1a5c1595dfd84893892f322ce3
daniel <danielrubensdaniel@gmail.com>
2023-04-26 15:12:16 -0300

    source: vehicle mock

---
+++ __tests__/utils/VehicleMock.ts
@@ -0,0 +1,16 @@
+import IVehicle from '../../src/Interfaces/IVehicle';
+
+export const validVehicle: IVehicle = {
+  model: 'anyvahicle',
+  year: 2000,
+  color: 'anycolor',
+  buyValue: 2000,
+};
+
+export const validVehicleWithStatus: IVehicle = {
+  model: 'anyvahicle',
+  year: 2000,
+  color: 'anycolor',
+  status: true,
+  buyValue: 2000,
+};
