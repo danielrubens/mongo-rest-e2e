2a9f20bebb4b760ac5654bdcbf84c89db6b6115b
daniel <danielrubensdaniel@gmail.com>
2023-05-04 18:06:33 -0300

    init swagger

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,44 @@
 import express from 'express';
+import swaggerJsDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 import vehicles from './Controllers/vehicles';
 
+// Extended: https://swagger.io/specification/#infoObject
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      title: 'Vehicles API',
+      description: 'Vehicles API Description',
+      contact: {
+        name: 'Daniel Rubens',
+      },
+      servers: ['http://localhost:3001'],       
+      version: '0.1',
+    },
+  },
+  // ['.routes/*.ts']   
+  apis: ['src/app.ts'],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+
 const app = express();
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use(express.Router());
 app.use(express.json());
 
+// Routes
+/** 
+ * @swagger
+ * /cars:
+ *  get:
+ *    description: Use descriptions
+ *    responses:
+ *      '200':
+ *        description: A successfull description
+*/
 app.get('/cars', vehicles.getAll);
+
 app.get('/cars/:id', vehicles.getById);
 app.post('/cars', vehicles.create);
 app.put('/cars/:id', vehicles.update);
