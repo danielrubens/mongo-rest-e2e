b76f059bb7ab66b337c469f27a7572ccee22600c
daniel <danielrubensdaniel@gmail.com>
2023-05-05 13:00:28 -0300

    feat: swagger docs

--- src/app.ts
+++ src/app.ts
@@ -35,17 +35,85 @@ app.use(express.json());
  *    description: Use descriptions
  *    responses:
  *      '200':
- *        description: A successfull description
+ *        description: Returns all cars on the database
 */
 app.get('/cars', vehicles.getAll);
 
+/** 
+ * @swagger
+ * /cars/:id:
+ *  get:
+ *    description: Get element by id
+ *    responses:
+ *      '200':
+ *        description: Returns element whose id is equal to the route parameter 
+*/
 app.get('/cars/:id', vehicles.getById);
+
+/** 
+ * @swagger
+ * /cars:
+ *  post:
+ *    description: Create an element
+ *    responses:
+ *      '201':
+ *        description: Returns element created 
+*/
 app.post('/cars', vehicles.create);
+
+/** 
+ * @swagger
+ * /cars/:id:
+ *  put:
+ *    description: Update an element
+ *    responses:
+ *      '200':
+ *        description: Returns element updated
+*/
 app.put('/cars/:id', vehicles.update);
 
+/** 
+ * @swagger
+ * /motorcycles:
+ *  get:
+ *    description: Use descriptions
+ *    responses:
+ *      '200':
+ *        description: Returns all motorcycles on the database
+*/
 app.get('/motorcycles', vehicles.getAll);
+
+/** 
+ * @swagger
+ * /motorcycles/:id:
+ *  get:
+ *    description: Get element by id
+ *    responses:
+ *      '200':
+ *        description: Returns element whose id is equal to the route parameter 
+*/
 app.get('/motorcycles/:id', vehicles.getById);
+
+/** 
+ * @swagger
+ * /motorcycles:
+ *  post:
+ *    description: Create an element
+ *    responses:
+ *      '201':
+ *        description: Returns element created 
+*/
 app.post('/motorcycles', vehicles.create);
+
+/** 
+ * @swagger
+ * /motorcycles/:id:
+ *  put:
+ *    description: Update an element
+ *    responses:
+ *      '200':
+ *        description: Returns element updated
+*/
 app.put('/motorcycles/:id', vehicles.update);
 
 export default app;
